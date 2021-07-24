cd5fe9c35ccb9daa849783dd8e55c9a4100e46ef
Rohan Bhatnagar <rohan.bha09@gmail.com>
2021-07-24 16:58:50 -0400

    db connected

--- src/Title.js
+++ src/Title.js
@@ -2,7 +2,19 @@ import React from 'react';
 import './Title.css';
 import logo from './sun.png';
 import './fonts/PlayfairDisplay-Italic.ttf';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import firebase from "./util/firebase.js";
 
+const createElement = () => {
+  const elementRef = firebase.database().ref("elements");
+  const name = "Rohan";
+  const element = {
+    name,
+    complete: false
+  };
+  elementRef.push(element);
+}
 
 function Title() {
   return (
@@ -10,6 +22,9 @@ function Title() {
       <img src = {logo} height = "75px" width = "75px" />
       <h1>Theia</h1>
       <img src = {logo} height = "75px" width = "75px" />
+      <Container>
+        <Button onClick={createElement}> Test </Button>
+      </Container>
     </div>
   )
 }
---
+++ src/util/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyA400M1E-1OXygWTSWjpcCj5GKsq1MWOVk",
+    authDomain: "mtsi-9efd9.firebaseapp.com",
+    projectId: "mtsi-9efd9",
+    storageBucket: "mtsi-9efd9.appspot.com",
+    messagingSenderId: "878914986318",
+    appId: "1:878914986318:web:db6863163feab27a5d7e1f",
+    measurementId: "G-7R6CL3S5R9"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
